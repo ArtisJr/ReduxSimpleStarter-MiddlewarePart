bdf191b148afa79c25076c85f9b21f39057e5357
Artis Coleman <artiscolemanjr@gmail.com>
2018-03-06 07:19:04 -0500

    Working through the course

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionsCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onFromSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
-        <input />
+      <form onSubmit={this.onFromSubmit} className="input-group">
+        <input
+          placeholder="Get a five-day forecast in your favorite cities"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
         <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary" />
+          <button type="submit" className="btn btn-secondary">
+            Submit
+          </button>
         </span>
       </form>
     );
